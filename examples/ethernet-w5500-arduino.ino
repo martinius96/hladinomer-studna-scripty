e5aed5df245e2053a44d9e9f4cedcc417b512db4
Martin <martinius96@gmail.com>
2020-03-26 20:26:31 +0100

    Update ethernet-w5500-arduino.ino

--- examples/ethernet-w5500-arduino.ino
+++ examples/ethernet-w5500-arduino.ino
@@ -1,11 +1,11 @@
- 
-/*|---------------------------------|*/
-/*|Projekt: Hladinomer              |*/
-/*|Autor: Martin Chlebovec          |*/
-/*|E-mail: martinius96@gmail.com    |*/
-/*|Web: https://arduino.php5.sk     |*/
-/*|Licencia pouzitia: MIT           |*/
-/*|---------------------------------|*/
+/*|-------------------------------------------------|*/
+/*|Projekt: Hladinomer - Arduino + Ethernet W5500   |*/
+/*|Autor: Martin Chlebovec                          |*/
+/*|E-mail: martinius96@gmail.com                    |*/
+/*|Web: https://arduino.php5.sk                     |*/
+/*|Licencia pouzitia: MIT                           |*/
+/*|Revízia: 26. Marec 2020                          |*/
+/*|-------------------------------------------------|*/
 
 #include <avr\wdt.h>
 #include <SPI.h>
@@ -15,21 +15,35 @@
 #define pinEcho       4
 #define maxVzdialenost 450
 NewPing sonar(pinTrigger, pinEcho, maxVzdialenost);
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-char server[] = "www.arduino.php5.sk";
-IPAddress ip(192, 168, 1, 101);
-EthernetClient client;
+char* host = "www.arduino.php5.sk";
+byte mac[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
+//IPAddress ip(192, 168, 0, 2);
+//IPAddress dnServer(192, 168, 0, 1);
+//IPAddress gateway(192, 168, 0, 1);
+//IPAddress subnet(255, 255, 255, 0);
 void setup() {
   Serial.begin(115200);
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    Ethernet.begin(mac, ip);
+    Ethernet.begin(mac);
+    //Ethernet.begin(mac, ip);
+    //Ethernet.begin(mac, ip, dns);
+    //Ethernet.begin(mac, ip, dns, gateway);
+    //Ethernet.begin(mac, ip, dns, gateway, subnet);
   }
   wdt_enable(WDTO_8S);
 }
 
 void loop() {
   wdt_reset();
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    Ethernet.begin(mac);
+    //Ethernet.begin(mac, ip);
+    //Ethernet.begin(mac, ip, dns);
+    //Ethernet.begin(mac, ip, dns, gateway);
+    //Ethernet.begin(mac, ip, dns, gateway, subnet);
+  }
   int vzdialenost = sonar.ping_cm();
   delay(50);
   if (vzdialenost > 0) {
@@ -42,28 +56,30 @@ void loop() {
     Serial.print("Vzdialenost medzi senzorom a predmetom je: ");
     Serial.print(vzdialenost);
     Serial.println(" cm.");
-    if (client.connect(server, 80)) {
-      String hodnota = String(vzdialenost);
-      Serial.println("Pripojenie uspesne na webserver, vykonavam request... ");
-      client.print("GET /studna/data.php?hodnota=");
-      client.print(hodnota);
-      client.println(" HTTP/1.1");
-      client.println("Host: www.arduino.php5.sk");
+    String data = "hodnota=" + String(vzdialenost);
+    String url = "/studna/data.php";
+    if (client.connect(host, 80)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println("User-Agent: EthernetW5500");
       client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded;");
+      client.print("Content-Length: ");
+      client.println(data.length());
       client.println();
+      client.println(data);
       Serial.println("Data uspesne odoslane na web");
-      client.stop();
     } else {
       Serial.println("Pripojenie zlyhalo...");
     }
-  }
-  else {
+    client.stop();
+    for (int i = 0; i <= 300; i++) {
+      delay(1000);
+      wdt_reset();
+    }
+  } else {
     Serial.println("Vzdialenost medzi predmetom a senzorom je mimo rozsah.");
     wdt_reset();
     delay(500);
-  }
-  for (int i = 0; i <= 300; i++) {
-    delay(1000);
-    wdt_reset();
   }
 }
