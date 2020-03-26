4c980190756fd435680778a60aa0e60808e809b3
Martin <martinius96@gmail.com>
2020-03-26 21:07:42 +0100

    Update https-esp32.ino

--- examples/https-esp32.ino
+++ examples/https-esp32.ino
@@ -1,11 +1,11 @@
-  
-/*|---------------------------------|*/
-/*|Projekt: Hladinomer              |*/
-/*|Autor: Martin Chlebovec          |*/
-/*|E-mail: martinius96@gmail.com    |*/
-/*|Web: https://arduino.php5.sk     |*/
-/*|Licencia pouzitia: MIT           |*/
-/*|---------------------------------|*/
+/*|-------------------------------------------------|*/
+/*|Projekt: Hladinomer - ESP32 - HTTPS              |*/
+/*|Autor: Martin Chlebovec                          |*/
+/*|E-mail: martinius96@gmail.com                    |*/
+/*|Web: https://arduino.php5.sk                     |*/
+/*|Licencia pouzitia: MIT                           |*/
+/*|Revízia: 26. Marec 2020                          |*/
+/*|-------------------------------------------------|*/
 
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
@@ -18,7 +18,6 @@ NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
 const char * ssid = "wifi_meno"; //meno wifi siete
 const char * password = "wifi_heslo"; //heslo na wifi siet
 const char * host = "arduino.php5.sk"; //bez https a bez www
-const int httpsPort = 443;
 
 const char* test_root_ca = \
                            "-----BEGIN CERTIFICATE-----\n" \
@@ -83,22 +82,29 @@ void loop() {
     Serial.print("Vzdialenost medzi senzorom a predmetom je: ");
     Serial.print(vzdialenost);
     Serial.println(" cm.");
-    if (client.connect(host, httpsPort)) {
-      String hodnota = String(vzdialenost);
-      Serial.println("Pripojenie uspesne na webserver, vykonavam request... ");
-      String url = "/studna/data.php?hodnota=" + hodnota;
-      client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: ESP32\r\n" + "Connection: close\r\n\r\n");
-      Serial.println("Hodnoty do databazy uspesne odoslane");
-      client.stop();
+    String data = "hodnota=" + String(vzdialenost);
+    String url = "/studna/data.php";
+    if (client.connect(host, 443)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println("User-Agent: ESP32");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded;");
+      client.print("Content-Length: ");
+      client.println(data.length());
+      client.println();
+      client.println(data);
+      Serial.println("Data uspesne odoslane na web");
     } else {
       Serial.println("Pripojenie zlyhalo...");
+    }
+    client.stop();
+    for (int i = 0; i <= 300; i++) {
+      delay(1000);
     }
   }
   else {
     Serial.println("Vzdialenost medzi predmetom a senzorom je mimo rozsah.");
     delay(500);
-  }
-  for (int i = 0; i <= 300; i++) {
-    delay(1000);
   }
 }
