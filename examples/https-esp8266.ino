c7403a7fdd93f824b97c2adaa60f1e069d51dd22
Martin <martinius96@gmail.com>
2020-03-26 20:53:36 +0100

    Update https-esp8266.ino

--- examples/https-esp8266.ino
+++ examples/https-esp8266.ino
@@ -1,11 +1,11 @@
-/*|---------------------------------|*/
-/*|Projekt: Hladinomer              |*/
-/*|Autor: Martin Chlebovec          |*/
-/*|E-mail: martinius96@gmail.com    |*/
-/*|Web: https://arduino.php5.sk     |*/
-/*|Licencia pouzitia: MIT           |*/
-/*|Arduino Core 2.5.0+              |*/
-/*|---------------------------------|*/
+/*|-------------------------------------------------|*/
+/*|Projekt: Hladinomer - ESP8266 - HTTP             |*/
+/*|Autor: Martin Chlebovec                          |*/
+/*|E-mail: martinius96@gmail.com                    |*/
+/*|Web: https://arduino.php5.sk                     |*/
+/*|Licencia pouzitia: MIT                           |*/
+/*|Revízia: 26. Marec 2020                          |*/
+/*|-------------------------------------------------|*/
 
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
@@ -15,12 +15,12 @@
 #define maxVzdialenost 450
 NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
 
+
 const char * ssid = "wifi_meno"; //meno wifi siete
 const char * password = "wifi_heslo"; //heslo na wifi siet
 const char * host = "arduino.php5.sk"; //bez https a bez www
-
 const char fingerprint[] PROGMEM = "b0 6d 7f 8c 98 78 8e 6e 0a 57 a8 2f 7e d1 40 2a 1e 3f 48 f7";
-const int httpsPort = 443; //https port
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
@@ -42,6 +42,8 @@ void loop() {
     Serial.print(".");
   }
   int vzdialenost = sonar.ping_cm();
+  WiFiClientSecure client;
+  client.setFingerprint(fingerprint);
   delay(50);
   if (vzdialenost > 0) {
     vzdialenost = 0;
@@ -53,24 +55,29 @@ void loop() {
     Serial.print("Vzdialenost medzi senzorom a predmetom je: ");
     Serial.print(vzdialenost);
     Serial.println(" cm.");
-    WiFiClientSecure client;
-    client.setFingerprint(fingerprint);
-    if (client.connect(host, httpsPort)) {
-      String hodnota = String(vzdialenost);
-      Serial.println("Pripojenie uspesne na webserver, vykonavam request... ");
-      String url = "/studna/data.php?hodnota=" + hodnota;
-      client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: NodeMCU\r\n" + "Connection: close\r\n\r\n");
-      Serial.println("Hodnoty do databazy uspesne odoslane");
-      client.stop();
+    String data = "hodnota=" + String(vzdialenost);
+    String url = "/studna/data.php";
+    if (client.connect(host, 443)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println("User-Agent: ESP8266");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded;");
+      client.print("Content-Length: ");
+      client.println(data.length());
+      client.println();
+      client.println(data);
+      Serial.println("Data uspesne odoslane na web");
     } else {
       Serial.println("Pripojenie zlyhalo...");
+    }
+    client.stop();
+    for (int i = 0; i <= 300; i++) {
+      delay(1000);
     }
   }
   else {
     Serial.println("Vzdialenost medzi predmetom a senzorom je mimo rozsah.");
     delay(500);
-  }
-  for (int i = 0; i <= 300; i++) {
-    delay(1000);
   }
 }
