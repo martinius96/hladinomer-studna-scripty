7c44b511d96eec55a055c1579bd5c5581c55db40
Martin Chlebovec <martinius96@gmail.com>
2020-08-12 21:38:54 +0200

    OTA ESP32

---
+++ examples/HTTP/OTA_ESP32/OTA_ESP32.ino
@@ -0,0 +1,121 @@
+/*|-------------------------------------------------|*/
+/*|Projekt: Hladinomer - ESP32 - HTTP - OTA         |*/
+/*|Autor: Martin Chlebovec                          |*/
+/*|E-mail: martinius96@gmail.com                    |*/
+/*|Web: https://arduino.php5.sk                     |*/
+/*|Licencia pouzitia: MIT                           |*/
+/*|Revízia: 12. August 2020                         |*/
+/*|-------------------------------------------------|*/
+
+#include <WiFi.h>
+#include <NewPingESP8266.h>
+#define pinTrigger    22
+#define pinEcho       23
+#define maxVzdialenost 450
+NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
+
+const char * ssid = "wifi_meno"; //meno wifi siete
+const char * password = "wifi_heslo"; //heslo na wifi siet
+const char * host = "www.arduino.php5.sk"; //bez https a bez www
+
+WiFiClient client;
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("");
+  Serial.println("Wifi pripojene s IP:");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  ArduinoOTA.handle();
+  int vzdialenost = sonar.ping_cm();
+  delay(50);
+  if (vzdialenost > 0) {
+    vzdialenost = 0;
+    for (int i = 0; i < 5; i++) {
+      vzdialenost += sonar.ping_cm();
+      delay(50);
+    }
+    vzdialenost = vzdialenost / 5;
+    Serial.print("Vzdialenost medzi senzorom a predmetom je: ");
+    Serial.print(vzdialenost);
+    Serial.println(" cm.");
+    String data = "hodnota=" + String(vzdialenost);
+    String url = "/studna/data.php";
+    if (client.connect(host, 80)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println("User-Agent: ESP32");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded;");
+      client.print("Content-Length: ");
+      client.println(data.length());
+      client.println();
+      client.println(data);
+      Serial.println("Data uspesne odoslane na web");
+    } else {
+      Serial.println("Pripojenie zlyhalo...");
+    }
+    client.stop();
+    for (int i = 0; i <= 300; i++) {
+      ArduinoOTA.handle();
+      delay(1000);
+    }
+  }
+  else {
+    Serial.println("Vzdialenost medzi predmetom a senzorom je mimo rozsah.");
+    delay(500);
+  }
+}
