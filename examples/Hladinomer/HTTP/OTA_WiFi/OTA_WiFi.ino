28b5dee6a675eee31eba37c2b916d65011db6167
Martin Chlebovec <martinius96@gmail.com>
2021-02-26 07:56:06 +0100

    Create OTA_WiFi.ino

---
+++ examples/Hladinomer/HTTP/OTA_WiFi/OTA_WiFi.ino
@@ -0,0 +1,147 @@
+/*|-----------------------------------------------------------------------------------|*/
+/*|Projekt: Hladinomer - HTTP - OTA - HC-SR04 / JSN-SR04T / HY-SRF05                  |*/
+/*|ESP8266 (NodeMCU, Wemos D1 Mini, Generic), ESP32 (DevKit, Generic)                 |*/
+/*|Autor: Bc. Martin Chlebovec (martinius96)                                          |*/
+/*|E-mail: martinius96@gmail.com                                                      |*/
+/*|Info k projektu (schéma): https://martinius96.github.io/hladinomer-studna-scripty/ |*/
+/*|Testovacie webove rozhranie: http://arduino.clanweb.eu/studna_s_prekladom/         |*/
+/*|Knižnica ESP8266NewPing je dostupná v Github repozitári:                           |*/
+/*|https://github.com/martinius96/hladinomer-studna-scripty/ - stihnuť a rozbaliť     |*/
+/*|Obsah priečinka /src/ nakopírovať do C:/Users/User/Dokumenty/Arduino/libraries/    |*/
+/*|Na toto webove rozhranie posiela mikrokontroler data                               |*/
+/*|Na zaklade zvolenej platformy v Arduino IDE sa vykona kompilacia podla direktiv    |*/
+/*|Licencia pouzitia: MIT                                                             |*/
+/*|Revízia: 26. Februar 2021                                                          |*/
+/*|-----------------------------------------------------------------------------------|*/
+
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <NewPingESP8266.h>
+
+const char * ssid = "WIFI_MENO_SIETE"; //MENO WiFi SIETE
+const char * password = "WIFI_HESLO_SIETE"; //HESLO WiFi SIETE
+const char* host = "arduino.clanweb.eu"; //adresa webservera (doména) na ktorú sa odosielajú dáta
+String url = "/studna_s_prekladom/data.php"; //URL adresa - cesta pod domenou k cieľovemu .php súboru, ktorý realizuje zápis
+//Pre testovacie webove rozhranie sa data odosielaju na: arduino.clanweb.eu/studna_s_prekladom/data.php (HTTP POST ONLY)
+unsigned long timer2 = 0;
+#if defined(ESP32)
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include "esp_system.h"
+#define pinTrigger    22
+#define pinEcho       23
+#define maxVzdialenost 450
+NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
+
+const int wdtTimeout = 30000;  //time in ms to trigger the watchdog
+hw_timer_t *timer = NULL;
+
+void IRAM_ATTR resetModule() {
+  ets_printf(F("reboot\n"));
+  esp_restart();
+}
+
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <NewPingESP8266.h>
+#define pinTrigger    5 //D1
+#define pinEcho       4 //D2
+#define maxVzdialenost 450
+NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
+#endif
+WiFiClient client;
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  ArduinoOTA.onStart([]() {
+    Serial.println(F("Start"));
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println(F("\nEnd"));
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println(F(""));
+  Serial.println(F("Wifi pripojene s IP:"));
+  Serial.println(WiFi.localIP());
+#if defined(ESP32)
+  timer = timerBegin(0, 80, true);                  //timer 0, div 80
+  timerAttachInterrupt(timer, &resetModule, true);  //attach callback
+  timerAlarmWrite(timer, wdtTimeout * 1000, false); //set time in us
+  timerAlarmEnable(timer);                          //enable interrupt
+#elif defined(ESP8266)
+  ESP.wdtEnable(30000);
+#endif
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+#if defined(ESP32)
+  timerWrite(timer, 0); //reset timer (feed watchdog)
+#elif defined(ESP8266)
+  ESP.wdtFeed();
+#endif
+  ArduinoOTA.handle();
+  yield();
+  if ((millis() - timer2) >= 300000 || timer2 == 0) {
+    timer2 = millis();
+    int vzdialenost = sonar.ping_cm();
+    delay(50);
+    if (vzdialenost > 0) {
+      vzdialenost = 0;
+      for (int i = 0; i < 10; i++) {
+        vzdialenost += sonar.ping_cm();
+        delay(50);
+      }
+      vzdialenost = vzdialenost / 10;
+      Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
+      Serial.print(vzdialenost);
+      Serial.println(F(" cm."));
+      String data = "hodnota=" + String(vzdialenost);
+      if (client.connect(host, 80)) {
+        client.println("POST " + url + " HTTP/1.0");
+        client.println("Host: " + (String)host);
+        client.println(F("User-Agent: ESP"));
+        client.println(F("Connection: close"));
+        client.println(F("Content-Type: application/x-www-form-urlencoded;"));
+        client.print(F("Content-Length: "));
+        client.println(data.length());
+        client.println();
+        client.println(data);
+        Serial.println(F("Data uspesne odoslane na web"));
+      } else {
+        Serial.println(F("Pripojenie zlyhalo..."));
+        delay(500);
+        timer2 = 0; //reset pocitadla, vykoname opakovany request
+      }
+    }
+    else {
+      Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
+      delay(500);
+      timer2 = 0; //reset pocitadla, vykoname opakovany request
+    }
+    client.stop();
+  }
+}
