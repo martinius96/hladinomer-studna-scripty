db49bb9ad1011d10c28757ce8d019583e4e40fab
Martin Chlebovec <martinius96@gmail.com>
2021-02-26 08:08:24 +0100

    Deep Sleep - WiFi modem OFF - Xtensa CPU SLEEP

---
+++ examples/Hladinomer/HTTP/ULP_WiFi/ULP_WiFi.ino
@@ -0,0 +1,96 @@
+/*|-----------------------------------------------------------------------------------|*/
+/*|Projekt: Hladinomer - HTTP - ULP - DEEP SLEEP - HC-SR04 / JSN-SR04T / HY-SRF05     |*/
+/*|ESP8266 (NodeMCU, Wemos D1 Mini, Generic), ESP32 (DevKit, Generic)                 |*/
+/*|Autor: Bc. Martin Chlebovec (martinius96)                                          |*/
+/*|E-mail: martinius96@gmail.com                                                      |*/
+/*|Info k projektu (schéma): https://martinius96.github.io/hladinomer-studna-scripty/ |*/
+/*|Testovacie webove rozhranie: http://arduino.clanweb.eu/studna_s_prekladom/         |*/
+/*|Knižnica ESP8266NewPing je dostupná v Github repozitári:                           |*/
+/*|https://github.com/martinius96/hladinomer-studna-scripty/ - stihnuť a rozbaliť     |*/
+/*|Obsah priečinka /src/ nakopírovať do C:/Users/User/Dokumenty/Arduino/libraries/    |*/
+/*|Na toto webove rozhranie posiela mikrokontroler data                               |*/
+/*|Na zaklade zvolenej platformy v Arduino IDE sa vykona kompilacia podla direktiv    |*/
+/*|Licencia pouzitia: MIT                                                             |*/
+/*|Revízia: 26. Februar 2021                                                          |*/
+/*|-----------------------------------------------------------------------------------|*/
+
+const char * ssid = "WIFI_MENO_SIETE"; //MENO WiFi SIETE
+const char * password = "WIFI_HESLO_SIETE"; //HESLO WiFi SIETE
+const char* host = "arduino.clanweb.eu"; //adresa webservera (doména) na ktorú sa odosielajú dáta
+String url = "/studna_s_prekladom/data.php"; //URL adresa - cesta pod domenou k cieľovemu .php súboru, ktorý realizuje zápis
+//Pre testovacie webove rozhranie sa data odosielaju na: arduino.clanweb.eu/studna_s_prekladom/data.php (HTTP POST ONLY)
+
+#include <NewPingESP8266.h>
+#if defined(ESP32)
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  300        /* Time ESP32 will go to sleep (in seconds) */
+#include <WiFi.h>
+#define pinTrigger    22
+#define pinEcho       23
+#define maxVzdialenost 450
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#define pinTrigger    5 //D1
+#define pinEcho       4 //D2
+#define maxVzdialenost 450
+#endif
+
+NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
+
+WiFiClient client;
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(F("."));
+  }
+  Serial.println(F(""));
+  Serial.println(F("Wifi pripojene s IP:"));
+  Serial.println(WiFi.localIP());
+  int vzdialenost = sonar.ping_cm();
+  delay(50);
+  if (vzdialenost > 0) {
+    vzdialenost = 0;
+    for (int i = 0; i < 10; i++) {
+      vzdialenost += sonar.ping_cm();
+      delay(50);
+    }
+    vzdialenost = vzdialenost / 10;
+    Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
+    Serial.print(vzdialenost);
+    Serial.println(F(" cm."));
+    String data = "hodnota=" + String(vzdialenost);
+    client.stop();
+    if (client.connect(host, 80)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println(F("User-Agent: ESP"));
+      client.println(F("Connection: close"));
+      client.println(F("Content-Type: application/x-www-form-urlencoded;"));
+      client.print(F("Content-Length: "));
+      client.println(data.length());
+      client.println();
+      client.println(data);
+      Serial.println(F("Data uspesne odoslane na web"));
+    } else {
+      Serial.println(F("Pripojenie zlyhalo..."));
+    }
+  }
+  else {
+    Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
+  }
+  client.stop();
+#if defined(ESP32)
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
+  esp_deep_sleep_start();
+#elif defined(ESP8266)
+  Serial.println(F("Entering deep sleep mode for 300 seconds"));
+  ESP.deepSleep(300e6);
+#endif
+}
+
+void loop() {
+
+}
