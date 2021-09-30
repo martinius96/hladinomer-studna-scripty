3029bbc091ec462a7220d3f59453808bbb5c3d82
Martin Chlebovec <martin.chlebovec.2@student.tuke.sk>
2021-09-30 15:08:52 +0200

    presun

---
+++ docs/MATLAB/HCSR04.m
@@ -0,0 +1,85 @@
+close all;
+clear all;
+hlbka1 = 0:0.01:0.5;
+hlbka2 = 0.5:0.01:1;
+hlbka3 = 1:0.01:1.5;
+hlbka4 = 1.5:0.01:2;
+hlbka5 = 2:0.01:2.5;
+hlbka6 = 2.5:0.01:3;
+hlbka7 = 3:0.01:3.5;
+hlbka8 = 3.5:0.01:4;
+hlbka9 = 4:0.01:4.5;
+priemer1 = hlbka1*tan(0.1316525)
+priemer2 = hlbka1*tan(-0.1316525)
+priemer3 = hlbka2*tan(0.1316525)
+priemer4 = hlbka2*tan(-0.1316525)
+priemer5 = hlbka3*tan(0.1316525)
+priemer6 = hlbka3*tan(-0.1316525)
+
+priemer7 = hlbka4*tan(0.1316525)
+priemer8 = hlbka4*tan(-0.1316525)
+
+priemer9 = hlbka5*tan(0.1316525)
+priemer10 = hlbka5*tan(-0.1316525)
+
+priemer11 = hlbka6*tan(0.1316525)
+priemer12 = hlbka6*tan(-0.1316525)
+
+priemer13 = hlbka7*tan(0.1316525)
+priemer14 = hlbka7*tan(-0.1316525)
+
+priemer15 = hlbka8*tan(0.1316525)
+priemer16 = hlbka8*tan(-0.1316525)
+
+priemer17 = hlbka9*tan(0.1316525)
+priemer18 = hlbka9*tan(-0.1316525)
+plot (hlbka1, priemer1, '-b')
+hold on 
+plot (hlbka1, priemer2, '-b')
+hold on 
+plot (hlbka2, priemer3, '-r')
+hold on 
+plot (hlbka2, priemer4, '-r')
+hold on 
+plot (hlbka3, priemer5, '-g')
+hold on 
+plot (hlbka3, priemer6, '-g')
+hold on 
+plot (hlbka4, priemer7, '-c')
+hold on 
+plot (hlbka4, priemer8, '-c')
+hold on 
+plot (hlbka5, priemer9, '-m')
+hold on 
+plot (hlbka5, priemer10, '-m')
+hold on 
+plot (hlbka6, priemer11, '-k')
+hold on 
+plot (hlbka6, priemer12, '-k')
+hold on 
+plot (hlbka7, priemer13, '-b')
+hold on 
+plot (hlbka7, priemer14, '-b')
+hold on 
+plot (hlbka8, priemer15, '-r')
+hold on 
+plot (hlbka8, priemer16, '-r')
+hold on 
+plot (hlbka9, priemer17, '-g')
+hold on 
+plot (hlbka9, priemer18, '-g')
+patch([hlbka1 fliplr(hlbka1)], [priemer1 fliplr(priemer2)], 'b')
+patch([hlbka2 fliplr(hlbka2)], [priemer3 fliplr(priemer4)], 'r')
+patch([hlbka3 fliplr(hlbka3)], [priemer5 fliplr(priemer6)], 'g')
+patch([hlbka4 fliplr(hlbka4)], [priemer7 fliplr(priemer8)], 'c')
+patch([hlbka5 fliplr(hlbka5)], [priemer9 fliplr(priemer10)], 'm')
+patch([hlbka6 fliplr(hlbka6)], [priemer11 fliplr(priemer12)], 'k')
+patch([hlbka7 fliplr(hlbka7)], [priemer13 fliplr(priemer14)], 'b')
+patch([hlbka8 fliplr(hlbka8)], [priemer15 fliplr(priemer16)], 'r')
+patch([hlbka9 fliplr(hlbka9)], [priemer17 fliplr(priemer18)], 'g')
+hold on
+plot ([0,4.5], [0,0], '-k', 'LineWidth',3)
+title('DetekËn· charakteristika senzora HC-SR04')
+grid minor
+xlabel('Vzdialenosù (m)') 
+ylabel('Polomer detekovanej plochy') 
