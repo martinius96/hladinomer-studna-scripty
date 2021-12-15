eb8cfb7cf6509fe499b0b644f69f54fd07e31eb7
Martin Chlebovec <martinius96@gmail.com>
2021-12-15 12:27:26 +0100

    Update OTA.ino

--- examples/Hladinomer/HTTPS/OTA/OTA.ino
+++ examples/Hladinomer/HTTPS/OTA/OTA.ino
@@ -1,31 +1,58 @@
 /*|------------------------------------------------------------------------------------|*/
 /*|Projekt: Hladinomer - HTTPS - OTA - HC-SR04 / JSN-SR04T / HY-SRF05                  |*/
 /*|ESP8266 (NodeMCU, Wemos D1 Mini, Generic), ESP32 (DevKit, Generic)                  |*/
-/*|Autor: Bc. Martin Chlebovec (martinius96)                                           |*/
+/*|Autor: Martin Chlebovec (martinius96)                                               |*/
 /*|E-mail: martinius96@gmail.com                                                       |*/
 /*|Info k projektu (schéma): https://martinius96.github.io/hladinomer-studna-scripty/  |*/
-/*|Testovacie webove rozhranie pre HTTP: http://arduino.clanweb.eu/studna_s_prekladom/ |*/
-/*|Testovacie webove rozhranie pre HTTPS: https://esp32.sk/studna_s_prekladom/         |*/
+/*|Testovacie webove rozhranie pre HTTPS: https://hladinomer.000webhostapp.com/        |*/
 /*|Knižnica ESP8266NewPing je dostupná v Github repozitári:                            |*/
 /*|https://github.com/martinius96/hladinomer-studna-scripty/ - stihnuť a rozbaliť      |*/
 /*|Obsah priečinka /src/ nakopírovať do C:/Users/User/Dokumenty/Arduino/libraries/     |*/
 /*|Na toto webove rozhranie posiela mikrokontroler data                                |*/
 /*|Na zaklade zvolenej platformy v Arduino IDE sa vykona kompilacia podla direktiv     |*/
 /*|Licencia pouzitia: MIT                                                              |*/
-/*|Revízia: 5. April 2021                                                              |*/
+/*|Revízia: 15. December 2021                                                          |*/
 /*|------------------------------------------------------------------------------------|*/
 
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <NewPingESP8266.h>
 #include <WiFiClientSecure.h>
-const char* host = "esp32.sk"; //adresa webservera (doména) na ktorú sa odosielajú dáta
-String url = "/studna_s_prekladom/data.php"; //URL adresa - cesta pod domenou k cieľovemu .php súboru, ktorý realizuje zápis
-//Pre testovacie webove rozhranie s HTTPS konektivitou data odosielaju na: esp32.sk/studna_s_prekladom/data.php (HTTP POST ONLY)
+const char* host = "hladinomer.000webhostapp.com"; //adresa webservera (doména) na ktorú sa odosielajú dáta
+String url = "/data.php"; //URL adresa - cesta pod domenou k cieľovemu .php súboru, ktorý realizuje zápis
 const char * ssid = "WIFI_NAME"; //MENO WiFi SIETE
+
 const char * password = "WIFI_PASSWORD"; //HESLO WiFi SIETE
 
-unsigned long timer2 = 0;
+
+//Root CA cert --> CERTIFIKÁT CERTIFIKAČNEJ AUTORITY, KTORÁ VYDALA CERTIFIKÁT VÁŠ WEBSERVER v .pem formáte
+//DST ROOT CA X3 EXAMPLE (https://i.imgur.com/fvw4huT.png)
+const static char* test_root_ca PROGMEM = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+    "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+    "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+    "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+    "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+    "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+    "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+    "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+    "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+    "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+    "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+    "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+    "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+    "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+    "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+    "-----END CERTIFICATE-----\n";
+
+
+
 #if defined(ESP32)
 #include <WiFi.h>
 #include <ESPmDNS.h>
@@ -33,32 +60,8 @@ unsigned long timer2 = 0;
 #define pinTrigger    22
 #define pinEcho       23
 #define maxVzdialenost 450
-NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
 const int wdtTimeout = 30000;  //time in ms to trigger the watchdog
 hw_timer_t *timer = NULL;
-
-//esp32.sk Root CA (vlastná OWN CA, self-signed).
-const static char* test_root_ca PROGMEM = \
-    "-----BEGIN CERTIFICATE-----\n" \
-    "MIIDTzCCAjcCFGoeGOBevdCzZX61CSkNsw1yLOgeMA0GCSqGSIb3DQEBCwUAMGQx\n" \
-    "CzAJBgNVBAYTAlVTMRMwEQYDVQQIDApGYWtlIFN0YXRlMRYwFAYDVQQHDA1GYWtl\n" \
-    "IExvY2FsaXR5MRUwEwYDVQQKDAxGYWtlIENvbXBhbnkxETAPBgNVBAMMCGVzcDMy\n" \
-    "LnNrMB4XDTE4MTIxNDE4MTcyNVoXDTIzMTIxMzE4MTcyNVowZDELMAkGA1UEBhMC\n" \
-    "VVMxEzARBgNVBAgMCkZha2UgU3RhdGUxFjAUBgNVBAcMDUZha2UgTG9jYWxpdHkx\n" \
-    "FTATBgNVBAoMDEZha2UgQ29tcGFueTERMA8GA1UEAwwIZXNwMzIuc2swggEiMA0G\n" \
-    "CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDeimA7p/eIaGtXPM2QyD3+tDI+6ltB\n" \
-    "bPzHUPupBK3EYi1bCtHGg0Lq8cbCdFVhJJnvRGoIdz/knhTIkMphMvTC5UJLooPx\n" \
-    "RnI9KSdXWs2n/wXYvPS9Txthz6zv0Ua9Dp+OcXvz0wzG1skhd+RPAm9zUJDJ42P1\n" \
-    "FIjcehxH93GgrHzXz6zklFt01/8IAcBt7gl8/qBnjJNA8SS0BoYgfRnNOvn6k1yk\n" \
-    "ZZtyfIp68NWlfdveMd8Ig2pUIuvTh7MyUFjumcDLqAs2J+SenUYHEcfZJK4Vmdfo\n" \
-    "bZam4hx8f0BGrxqDhDMuTmh/Jqa8ZYANfR7ctD1DU6u7aceGYmSgCiNpAgMBAAEw\n" \
-    "DQYJKoZIhvcNAQELBQADggEBACA5n68AsjspvR3N33jF83FZqDXuvQbmY7lBm2nN\n" \
-    "Yh2265f9mRGrOO3z6lHKUfs5BJXxxahAKPQ07H9zQuYvIW0y8Jcgs4FlFhAuqgss\n" \
-    "KWuqJicf/noRPq/MC7mTLSGVrCo3R3x8U842j/Jdi0Ygb7AgFMp5F7DoS88QEMuK\n" \
-    "2UbJSA0czc24NImg9Y8Ve6VS4m+lsjsl77L15JCuDeZ7UBegmYoDmEIC/MohSD5t\n" \
-    "goX0+fCH4EsIkd6Zh8P6GTlRCglyGrSFgKTV/6HPSptAFvMFUofnfedCYJXrkjKc\n" \
-    "psVaEdVBIz/a4jcB6mkQQm06+KusHmwsc/VNYLD6+pjmIE0=\n" \
-    "-----END CERTIFICATE-----\n";
 
 void IRAM_ATTR resetModule() {
   ets_printf("reboot\n");
@@ -73,9 +76,11 @@ void IRAM_ATTR resetModule() {
 #define pinTrigger    5 //D1
 #define pinEcho       4 //D2
 #define maxVzdialenost 450
-NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
-const char fingerprint[] PROGMEM = "‎02 5b 13 b3 a6 d8 4c f3 b1 21 58 aa ab 3f 9f 69 2e 27 14 66"; //https://esp32.sk/studna_s_prekladom/
+X509List cert(test_root_ca);
 #endif
+NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
+unsigned long timer2 = 0;
+
 WiFiClientSecure client;
 void setup() {
   Serial.begin(115200);
@@ -112,8 +117,23 @@ void setup() {
   timerAlarmWrite(timer, wdtTimeout * 1000, false); //set time in us
   timerAlarmEnable(timer);                          //enable interrupt
 #elif defined(ESP8266)
-  //client.setFingerprint(fingerprint); //DOCASNE
-  client.setInsecure();
+  // Set time via NTP, as required for x.509 validation
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+  Serial.printf("Using certificate: %s\n", test_root_ca);
+  client.setTrustAnchors(&cert);
   ESP.wdtEnable(30000);
 #endif
 }
