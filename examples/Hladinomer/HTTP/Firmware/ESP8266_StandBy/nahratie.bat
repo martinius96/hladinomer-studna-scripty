93e68fc639ef2a98da5abda1b2f8c8b643587ec5
Martin Chlebovec <martinius96@gmail.com>
2021-12-23 17:58:40 +0100

    Add files via upload

---
+++ examples/Hladinomer/HTTP/Firmware/ESP8266_StandBy/nahratie.bat
@@ -0,0 +1,1 @@
+start esptool.exe --chip esp32 --port COM3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 sketch_sep27c.ino.bin 0x8000 sketch_sep27c.ino.partitions.bin
