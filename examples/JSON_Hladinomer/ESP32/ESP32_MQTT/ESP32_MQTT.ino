79cba4dd4f59e9614ece09ec83397e3710b37e50
Martin Chlebovec <martinius96@gmail.com>
2022-01-07 19:26:05 +0100

    Update ESP32_MQTT.ino

--- examples/JSON_Hladinomer/ESP32/ESP32_MQTT/ESP32_MQTT.ino
+++ examples/JSON_Hladinomer/ESP32/ESP32_MQTT/ESP32_MQTT.ino
@@ -6,19 +6,104 @@
 /*|Broker pre vývojárov IoT Industies Slovakia      |*/
 /*|Autor: Martin Chlebovec                          |*/
 /*|E-mail: martinius96@gmail.com                    |*/
-/*|Revízia: 9. Marec 2021                           |*/
+/*|Revízia: 7. Januar 2022                          |*/
 /*|-------------------------------------------------|*/
 
+//CHOOSE PROTOCOL FOR CONNECTION TO WEBSERVER
+#define HTTP
+//#define HTTPS
+#if defined HTTP && defined HTTPS
+#error HTTP and HTTPS cannot used in same time. Use only one definition and comment other (unsued) definiton of protocol
+#endif
+//CHOOSE PROTOCOL FOR CONNECTION TO MQTT BROKER FOR TOPIC DATA PUBLISH / SUBSCRIBE
+#define MQTT
+//#define MQTTS
+#if defined MQTT && defined MQTTS
+#error MQTT and MQTTS cannot used in same time. Use only one definition and comment other (unsued) definiton of protocol
+#endif
 #include <WiFi.h>
+#if defined HTTPS
+#include <WiFiClientSecure.h>
+#endif
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 const char* ssid = "SSID_NAME"; //SSID vasej WiFi siete
 const char* password = "SSID_PASSWORD"; //HESLO vasej WiFi siete
 const char* host = "arduino.clanweb.eu"; //IP adresa / domena webservera
 const char* mqtt_server = "mqtt.iotindustries.sk";
-const int httpPort = 80;
-WiFiClient klient; //pre HTTP
-WiFiClient espClient; //pre MQTT
+// Root CA for webserver domain
+const static char* webserver_root_ca PROGMEM = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+    "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+    "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+    "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+    "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+    "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+    "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+    "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+    "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+    "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+    "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+    "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+    "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+    "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+    "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+    "-----END CERTIFICATE-----\n";
+
+// Root CA for MQTT Broker domain
+const static char* mqtt_root_ca PROGMEM = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIFdzCCBF+gAwIBAgIQE+oocFv07O0MNmMJgGFDNjANBgkqhkiG9w0BAQwFADBv\n" \
+    "MQswCQYDVQQGEwJTRTEUMBIGA1UEChMLQWRkVHJ1c3QgQUIxJjAkBgNVBAsTHUFk\n" \
+    "ZFRydXN0IEV4dGVybmFsIFRUUCBOZXR3b3JrMSIwIAYDVQQDExlBZGRUcnVzdCBF\n" \
+    "eHRlcm5hbCBDQSBSb290MB4XDTAwMDUzMDEwNDgzOFoXDTIwMDUzMDEwNDgzOFow\n" \
+    "gYgxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpOZXcgSmVyc2V5MRQwEgYDVQQHEwtK\n" \
+    "ZXJzZXkgQ2l0eTEeMBwGA1UEChMVVGhlIFVTRVJUUlVTVCBOZXR3b3JrMS4wLAYD\n" \
+    "VQQDEyVVU0VSVHJ1c3QgUlNBIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MIICIjAN\n" \
+    "BgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAgBJlFzYOw9sIs9CsVw127c0n00yt\n" \
+    "UINh4qogTQktZAnczomfzD2p7PbPwdzx07HWezcoEStH2jnGvDoZtF+mvX2do2NC\n" \
+    "tnbyqTsrkfjib9DsFiCQCT7i6HTJGLSR1GJk23+jBvGIGGqQIjy8/hPwhxR79uQf\n" \
+    "jtTkUcYRZ0YIUcuGFFQ/vDP+fmyc/xadGL1RjjWmp2bIcmfbIWax1Jt4A8BQOujM\n" \
+    "8Ny8nkz+rwWWNR9XWrf/zvk9tyy29lTdyOcSOk2uTIq3XJq0tyA9yn8iNK5+O2hm\n" \
+    "AUTnAU5GU5szYPeUvlM3kHND8zLDU+/bqv50TmnHa4xgk97Exwzf4TKuzJM7UXiV\n" \
+    "Z4vuPVb+DNBpDxsP8yUmazNt925H+nND5X4OpWaxKXwyhGNVicQNwZNUMBkTrNN9\n" \
+    "N6frXTpsNVzbQdcS2qlJC9/YgIoJk2KOtWbPJYjNhLixP6Q5D9kCnusSTJV882sF\n" \
+    "qV4Wg8y4Z+LoE53MW4LTTLPtW//e5XOsIzstAL81VXQJSdhJWBp/kjbmUZIO8yZ9\n" \
+    "HE0XvMnsQybQv0FfQKlERPSZ51eHnlAfV1SoPv10Yy+xUGUJ5lhCLkMaTLTwJUdZ\n" \
+    "+gQek9QmRkpQgbLevni3/GcV4clXhB4PY9bpYrrWX1Uu6lzGKAgEJTm4Diup8kyX\n" \
+    "HAc/DVL17e8vgg8CAwEAAaOB9DCB8TAfBgNVHSMEGDAWgBStvZh6NLQm9/rEJlTv\n" \
+    "A73gJMtUGjAdBgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/\n" \
+    "BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wEQYDVR0gBAowCDAGBgRVHSAAMEQGA1Ud\n" \
+    "HwQ9MDswOaA3oDWGM2h0dHA6Ly9jcmwudXNlcnRydXN0LmNvbS9BZGRUcnVzdEV4\n" \
+    "dGVybmFsQ0FSb290LmNybDA1BggrBgEFBQcBAQQpMCcwJQYIKwYBBQUHMAGGGWh0\n" \
+    "dHA6Ly9vY3NwLnVzZXJ0cnVzdC5jb20wDQYJKoZIhvcNAQEMBQADggEBAJNl9jeD\n" \
+    "lQ9ew4IcH9Z35zyKwKoJ8OkLJvHgwmp1ocd5yblSYMgpEg7wrQPWCcR23+WmgZWn\n" \
+    "RtqCV6mVksW2jwMibDN3wXsyF24HzloUQToFJBv2FAY7qCUkDrvMKnXduXBBP3zQ\n" \
+    "YzYhBx9G/2CkkeFnvN4ffhkUyWNnkepnB2u0j4vAbkN9w6GAbLIevFOFfdyQoaS8\n" \
+    "Le9Gclc1Bb+7RrtubTeZtv8jkpHGbkD4jylW6l/VXxRTrPBPYer3IsynVgviuDQf\n" \
+    "Jtl7GQVoP7o81DgGotPmjw7jtHFtQELFhLRAlSv0ZaBIefYdgWOWnU914Ph85I6p\n" \
+    "0fKtirOMxyHNwu8=\n" \
+    "-----END CERTIFICATE-----\n";
+#if defined HTTP
+  const int httpPort = 80;
+  WiFiClient klient; //pre HTTP
+#endif
+#if defined HTTPS
+  const int httpPort = 443;
+  WiFiClientSecure klient; //pre HTTPS
+#endif
+#if defined MQTT
+  WiFiClient espClient; //pre MQTT
+#endif
+#if defined MQTTS
+  WiFiClientSecure espClient; //pre MQTTS
+#endif
 PubSubClient client(espClient);
 unsigned long timer = 0;
 int interval = 60000;
@@ -41,7 +126,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP32_Hladinomer_Client")) {
+    if (client.connect("ESP32_JSON_Client")) {
       Serial.println("connected");
       // Subscribe
       client.subscribe("hladinomer/#");
@@ -68,7 +153,18 @@ void setup() {
   Serial.println("");
   Serial.println("Wifi pripojene s IP:");
   Serial.println(WiFi.localIP());
-  client.setServer(mqtt_server, 1883);
+#if defined HTTPS
+  klient.setCACert(webserver_root_ca);
+#endif
+#if defined MQTTS
+  espClient.setCACert(mqtt_root_ca);
+#endif
+#if defined MQTT
+  client.setServer(mqtt_server, 1883); //MQTT port
+#endif
+#if defined MQTTS
+  client.setServer(mqtt_server, 8883); //MQTTS port
+#endif
   client.setCallback(callback);
   if (!client.connected()) {
     reconnect();
@@ -111,14 +207,14 @@ void loop() {
       char pole_vyska[32];
       dtostrf(vyska, 1, 2, pole_vyska);
       client.publish("hladinomer/vyska", pole_vyska);
-      Serial.print("Objem studne: ");
+      Serial.print(F("Objem studne: "));
       Serial.print(objem);
-      Serial.println(" litrov");
+      Serial.println(F(" litrov"));
       char pole_objem[32];
       dtostrf(objem, 1, 2, pole_objem);
       client.publish("hladinomer/objem", pole_objem);
     } else if (!klient.connect(host, httpPort)) {
-      Serial.println("Nepodarilo sa pripojenie k hladinomeru, ani nacitanie JSON dat");
+      Serial.println(F("Nepodarilo sa pripojenie k hladinomeru, ani nacitanie JSON dat"));
     }
   }
 }
