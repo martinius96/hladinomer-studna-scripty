195bcf134cf1c2b0a2a2f1c5ab69f648ca2207f6
Martin Chlebovec <martinius96@gmail.com>
2022-06-24 11:14:15 +0200

    Update StandBy.ino

--- examples/Hladinomer/HTTP/StandBy/StandBy.ino
+++ examples/Hladinomer/HTTP/StandBy/StandBy.ino
@@ -1,7 +1,7 @@
 /*|-----------------------------------------------------------------------------------|*/
 /*|Projekt: Hladinomer - HTTP - Compact - HC-SR04 / JSN-SR04T / HY-SRF05              |*/
 /*|Arduino + Ethernet (W5100 / W5500, ENC28J60), ESP8266 (NodeMCU), ESP32 (DevKit)    |*/
-/*|Autor: Bc. Martin Chlebovec (martinius96)                                          |*/
+/*|Autor: Martin Chlebovec (martinius96)                                              |*/
 /*|E-mail: martinius96@gmail.com                                                      |*/
 /*|Info k projektu (schéma): https://martinius96.github.io/hladinomer-studna-scripty/ |*/
 /*|Testovacie webove rozhranie: http://arduino.clanweb.eu/studna_s_prekladom/         |*/
@@ -11,61 +11,49 @@
 /*|Na toto webove rozhranie posiela mikrokontroler data                               |*/
 /*|Na zaklade zvolenej platformy v Arduino IDE sa vykona kompilacia podla direktiv    |*/
 /*|Licencia pouzitia: MIT                                                             |*/
-/*|Revízia: 19. Februar 2021                                                          |*/
+/*|Revízia: 24. Jun 2022                                                          |*/
 /*|-----------------------------------------------------------------------------------|*/
 
 const char* host = "arduino.clanweb.eu"; //adresa webservera (doména) na ktorú sa odosielajú dáta
 String url = "/studna_s_prekladom/data.php"; //URL adresa - cesta pod domenou k cieľovemu .php súboru, ktorý realizuje zápis
 //Pre testovacie webove rozhranie sa data odosielaju na: arduino.clanweb.eu/studna_s_prekladom/data.php (HTTP POST ONLY)
-
-//Kompatibilne mikrokontrolery z rady Arduino: Uno, Nano, Mega (R3)
+#define maxVzdialenost 450
+unsigned long timer2 = 0;
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 //HLAVICKOVE SUBORY PRE ARDUINO A ETHERNET SHIELD + watchdog
 #include <avr\wdt.h>
 #include <SPI.h>
-
-
-
-//VOLBA ETHERNET MODULU / SHIELDU K ARDUINU Z RADY WIZNET / ENC (Microchip)
-///////////////////////////////////////////////////////////////////////////////////////
-#include <Ethernet.h> //Ethernet shield Wiznet W5100 - zakomentovat ak sa nepouziva    
-//#include <Ethernet2.h> //Ethernet modul Wiznet W5500 - zakomentovat ak sa nepouziva   
-//#include <Ethernet3.h> //Ethernet modul WIZ850io / USR-ES1 (Wiznet W5500 V2) - zakomentovat ak sa nepouziva   
-//#include <UIPEthernet.h> //Ethernet modul ENC28J60 - zakomentovat ak sa nepouziva   
-///////////////////////////////////////////////////////////////////////////////////////
-
-    
-
-//PREMENNE, HLAVICKOVY SUBOR, OBJEKT PRE HC-SR04 / JSN-SR04T
+#include <Ethernet.h> //Ethernet shield Wiznet W5100 - zakomentovat ak sa nepouziva
+//#include <Ethernet2.h> //Ethernet modul Wiznet W5500 - zakomentovat ak sa nepouziva
+//#include <Ethernet3.h> //Ethernet modul WIZ850io / USR-ES1 (Wiznet W5500 V2) - zakomentovat ak sa nepouziva
+//#include <UIPEthernet.h> //Ethernet modul ENC28J60 - zakomentovat ak sa nepouziva
 #include <NewPing.h>
 #define pinTrigger    5
 #define pinEcho       6
-#define maxVzdialenost 450
 NewPing sonar(pinTrigger, pinEcho, maxVzdialenost);
-
 byte mac[] = { 0xAA, 0xBB, 0xCA, 0xDC, 0xEE, 0xDE };
 //IPAddress ip(192, 168, 0, 2);
 //IPAddress dnServer(192, 168, 0, 1);
 //IPAddress gateway(192, 168, 0, 1);
 //IPAddress subnet(255, 255, 255, 0);
-unsigned long timer = 0;
 EthernetClient client;
-
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
+#endif
+#if defined(ESP32) || defined(ESP8266)
 #include <NewPingESP8266.h>
+const char * ssid = "WIFI_MENO_SIETE"; //MENO WiFi SIETE
+const char * password = "WIFI_HESLO_SIETE"; //HESLO WiFi SIETE
+#endif
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
 #define pinTrigger    5 //D1
 #define pinEcho       4 //D2
-#define maxVzdialenost 450
 NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
-
-#elif defined(ESP32)
+#endif
+#if defined(ESP32)
 #include <WiFi.h>
-#include <NewPingESP8266.h>
 #include "esp_system.h"
 #define pinTrigger    22 //D22
 #define pinEcho       23 //D23
-#define maxVzdialenost 450
 NewPingESP8266 sonar(pinTrigger, pinEcho, maxVzdialenost);
 const int wdtTimeout = 30000;  //time in ms to trigger the watchdog
 hw_timer_t *timer = NULL;
@@ -75,15 +63,9 @@ void IRAM_ATTR resetModule() {
   esp_restart();
 }
 #endif
-
 #if defined(ESP32) || defined(ESP8266)
-const char * ssid = "WIFI_MENO_SIETE"; //MENO WiFi SIETE
-const char * password = "WIFI_HESLO_SIETE"; //HESLO WiFi SIETE
-
 WiFiClient client;
-unsigned long timer2 = 0;
 #endif
-
 void setup() {
   Serial.begin(115200);
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
@@ -93,7 +75,7 @@ void setup() {
     //Ethernet.begin(mac, ip); //pre staticku IPv4
     //Ethernet.begin(mac, ip, dns); //pre staticku IPv4
     //Ethernet.begin(mac, ip, dns, gateway); //pre staticku IPv4
-    //Ethernet.begin(mac, ip, dns, gateway, subnet); //pre staticku IPv4 
+    //Ethernet.begin(mac, ip, dns, gateway, subnet); //pre staticku IPv4
     //IBA PRE Ethernet3.h !!! POZOR --> INY BEGIN ORDER PARAMETROV.... : //Ethernet.begin(mac, ip, subnet, gateway, dns);
   }
   Serial.print(F("Priradena IP: "));
@@ -102,7 +84,7 @@ void setup() {
 #elif defined(ESP32) || defined(ESP8266)
   WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
     Serial.print(F("."));
   }
   Serial.println("");
@@ -122,180 +104,69 @@ void setup() {
 void loop() {
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
   wdt_reset(); //Feed watchdog
-  /* if (Ethernet.begin(mac) == 0) {
-    Ethernet.begin(mac); // pre DHCP
-    //Ethernet.begin(mac, ip); //pre staticku IPv4
-    //Ethernet.begin(mac, ip, dns); //pre staticku IPv4
-    //Ethernet.begin(mac, ip, dns, gateway); //pre staticku IPv4
-    //Ethernet.begin(mac, ip, dns, gateway, subnet);
-  } */
-
-  if ((millis() - timer) >= 300000 || timer == 0) { //rutina raz za 5 minut
-    timer = millis();
-    //NEPOUZIVAT MOMENTALNE, SPOSOBUJE PROBLEMY A ZACYKLENIE SIETE
-   // Ethernet.maintain(); //pre DHCP, pri statickej IP zakomentovat
-    int vzdialenost = sonar.ping_cm();
-    delay(50);
-    if (vzdialenost > 0) {
-      vzdialenost = 0;
-      for (int i = 0; i < 10; i++) {
-        vzdialenost += sonar.ping_cm();
-        delay(50);
-      }
-      wdt_reset();
-      vzdialenost = vzdialenost / 10;
-      Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
-      Serial.print(vzdialenost);
-      Serial.println(F(" cm."));
-      client.stop(); //ukoncenie vsetkych existujucich spojeni
-      String data = "hodnota=" + String(vzdialenost) + "&token=123456789";
-      if (client.connect(host, 80)) {
-        client.println("POST " + url + " HTTP/1.0");
-        client.println("Host: " + (String)host);
-        client.println(F("User-Agent: EthernetW5100")); //Moznost pouzit iny USER agent
-        client.println(F("Connection: close"));
-        client.println(F("Content-Type: application/x-www-form-urlencoded;"));
-        client.print(F("Content-Length: "));
-        client.println(data.length());
-        client.println();
-        client.println(data);
-        Serial.println(F("Data uspesne odoslane na webove rozhranie"));
-        while (client.connected()) {
-          String line = client.readStringUntil('\n');
-          if (line == "\r") {
-            break;
-          }
-        }
-        String line = client.readStringUntil('\n');
-      } else {
-        Serial.println(F("Pripojenie zlyhalo..."));
-        delay(500);
-        timer = 0;  //vynulujeme timer, znovu nameriame a vykoname request
-      }
-      client.stop();
-    } else {
-      Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
-      delay(500);
-      timer = 0; //vynulujeme timer, znovu nameriame a vykoname request
-    }
-  }
 #endif
 #if defined(ESP32) || defined(ESP8266)
+  yield();
   if (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
   }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
 #endif
 #if defined(ESP8266)
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(F("."));
-  }
   ESP.wdtFeed(); //feed watchdog
-  yield(); //nechaj ESP dokoncit WiFi stack
-  if ((millis() - timer2) >= 300000 || timer2 == 0) {
-    timer2 = millis();
-    int vzdialenost = sonar.ping_cm();
-    delay(50);
-    if (vzdialenost > 0) {
-      vzdialenost = 0;
-      for (int i = 0; i < 10; i++) {
-        vzdialenost += sonar.ping_cm();
-        delay(50);
-      }
-      vzdialenost = vzdialenost / 10;
-      Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
-      Serial.print(vzdialenost);
-      Serial.println(F(" cm."));
-      client.stop();
-      String data = "hodnota=" + String(vzdialenost) + "&token=123456789";
-      if (client.connect(host, 80)) {
-        client.println("POST " + url + " HTTP/1.0");
-        client.println("Host: " + (String)host);
-        client.println(F("User-Agent: ESP8266"));
-        client.println(F("Connection: close"));
-        client.println(F("Content-Type: application/x-www-form-urlencoded;"));
-        client.print(F("Content-Length: "));
-        client.println(data.length());
-        client.println();
-        client.println(data);
-        Serial.println(F("Data uspesne odoslane na web"));
-        while (client.connected()) {
-          String line = client.readStringUntil('\n');
-          if (line == "\r") {
-            break;
-          }
-        }
-        String line = client.readStringUntil('\n');
-      } else {
-        Serial.println(F("Pripojenie zlyhalo..."));
-        delay(500);
-        timer2 = 0;  //vynulujeme timer, znovu nameriame a vykoname request
-      }
-      client.stop();
-    }
-    else {
-      Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
-      delay(500);
-      client.stop();
-      timer2 = 0; //vynulujeme timer, znovu nameriame a vykoname request
-    }
-  }
-
-#elif defined(ESP32)
-  while (WiFi.status() != WL_CONNECTED) {
-    timerWrite(timer, 0); //reset timer (feed watchdog)
-    delay(500);
-    Serial.print(F("."));
-  }
-  timerWrite(timer, 0); //reset timer (feed watchdog)
-  yield(); //nechaj ESP dokoncit WiFi stack
-  if ((millis() - timer2) >= 300000 || timer2 == 0) {
-    timer2 = millis();
-    int vzdialenost = sonar.ping_cm();
-    delay(50);
-    if (vzdialenost > 0) {
-      vzdialenost = 0;
-      for (int i = 0; i < 10; i++) {
-        vzdialenost += sonar.ping_cm();
-        delay(50);
-      }
-      vzdialenost = vzdialenost / 10;
-      Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
-      Serial.print(vzdialenost);
-      Serial.println(F(" cm."));
-      client.stop();
-      String data = "hodnota=" + String(vzdialenost) + "&token=123456789";
-      if (client.connect(host, 80)) {
-        client.println("POST " + url + " HTTP/1.0");
-        client.println("Host: " + (String)host);
-        client.println(F("User-Agent: ESP32"));
-        client.println(F("Connection: close"));
-        client.println(F("Content-Type: application/x-www-form-urlencoded;"));
-        client.print(F("Content-Length: "));
-        client.println(data.length());
-        client.println();
-        client.println(data);
-        Serial.println(F("Data uspesne odoslane na web"));
-        while (client.connected()) {
-          String line = client.readStringUntil('\n');
-          if (line == "\r") {
-            break;
-          }
-        }
-        String line = client.readStringUntil('\n');
-      } else {
-        Serial.println(F("Pripojenie zlyhalo..."));
-        delay(500);
-        timer2 = 0;  //vynulujeme timer, znovu nameriame a vykoname request
-      }
-      client.stop();
-    }
-    else {
-      Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
-      delay(500);
-      client.stop();
-      timer2 = 0; //vynulujeme timer, znovu nameriame a vykoname request
-    }
-  }
 #endif
+#if defined(ESP32)
+  timerWrite(timer, 0); //reset timer (feed watchdog)
+#endif
+  if ((millis() - timer2) >= 300000 || timer2 == 0) {
+    timer2 = millis();
+    int vzdialenost = sonar.ping_cm();
+    delay(50);
+    if (vzdialenost > 0) {
+      vzdialenost = 0;
+      for (int i = 0; i < 10; i++) {
+        vzdialenost += sonar.ping_cm();
+        delay(50);
+      }
+      vzdialenost = vzdialenost / 10;
+      Serial.print(F("Vzdialenost medzi senzorom a predmetom je: "));
+      Serial.print(vzdialenost);
+      Serial.println(F(" cm."));
+      client.stop();
+      String data = "hodnota=" + String(vzdialenost) + "&token=123456789";
+      if (client.connect(host, 80)) {
+        client.println("POST " + url + " HTTP/1.0");
+        client.println("Host: " + (String)host);
+        client.println(F("User-Agent: MCU"));
+        client.println(F("Connection: close"));
+        client.println(F("Content-Type: application/x-www-form-urlencoded;"));
+        client.print(F("Content-Length: "));
+        client.println(data.length());
+        client.println();
+        client.println(data);
+        Serial.println(F("Data uspesne odoslane na web"));
+        while (client.connected()) {
+          String line = client.readStringUntil('\n');
+          if (line == "\r") {
+            break;
+          }
+        }
+        String line = client.readStringUntil('\n');
+      } else {
+        Serial.println(F("Pripojenie zlyhalo..."));
+        delay(500);
+        timer2 = 0;  //vynulujeme timer, znovu nameriame a vykoname request
+      }
+      client.stop();
+    }
+    else {
+      Serial.println(F("Vzdialenost medzi predmetom a senzorom je mimo rozsah."));
+      delay(500);
+      client.stop();
+      timer2 = 0; //vynulujeme timer, znovu nameriame a vykoname request
+    }
+  }
 }
