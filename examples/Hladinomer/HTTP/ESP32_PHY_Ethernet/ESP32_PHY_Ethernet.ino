22a9aa309c61d142ffc1af89c6c9294710b045e4
Martin Chlebovec <martinius96@gmail.com>
2022-09-07 13:33:13 +0200

    Update ESP32_PHY_Ethernet.ino

--- examples/Hladinomer/HTTP/ESP32_PHY_Ethernet/ESP32_PHY_Ethernet.ino
+++ examples/Hladinomer/HTTP/ESP32_PHY_Ethernet/ESP32_PHY_Ethernet.ino
@@ -6,7 +6,7 @@
 /*|Buy me a coffee at: paypal.me/chlebovec                                     |*/
 /*|Project info: https://martinius96.github.io/hladinomer-studna-scripty/en    |*/
 /*|Test web interface: http://arduino.clanweb.eu/studna_s_prekladom/?lang=en   |*/
-/*|Revision: 21. August 2022                                                    |*/
+/*|Revision: 21. August 2022                                                   |*/
 /*|----------------------------------------------------------------------------|*/
 
 #include <ETH.h>
