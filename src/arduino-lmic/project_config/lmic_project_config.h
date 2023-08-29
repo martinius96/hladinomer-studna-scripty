55ba0bed6035b3349417705dfb5a1c28b4321649
Martin Chlebovec <martinius96@gmail.com>
2023-08-29 18:07:22 +0200

    ArduinoLMIC

---
+++ src/arduino-lmic/project_config/lmic_project_config.h
@@ -0,0 +1,15 @@
+// project-specific definitions
+//#define CFG_eu868 1
+#define CFG_as923 1
+//#define CFG_au915 1
+//#define CFG_as923 1
+// #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP      /* for as923-JP; also define CFG_as923 */
+//#define CFG_kr920 1
+//#define CFG_in866 1
+#define CFG_sx1276_radio 1
+//#define LMIC_USE_INTERRUPTS
+#define DISABLE_PING
+#define DISABLE_BEACONS
+
+#define LMIC_DEBUG_LEVEL 2
+#define DISABLE_JOIN
