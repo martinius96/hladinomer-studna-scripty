831bb0c1fffb1538b1ebc12718a41a2105adb683
Martin Chlebovec <martinius96@gmail.com>
2026-03-31 17:46:17 +0200

    Add simulation project link to pico.ino
    Added simulation project link for Wokwi.

---
+++ examples/Hladinomer/Simulation_Wokwi/Pico_Arduino/pico.ino
@@ -0,0 +1,167 @@
+/*|-----------------------------------------------------------------|*/
+/*|Project: Water level monitor - HTTPS - ULP - Rpi Pico W (RP2040) |*/
+/*|Project info: https://your-iot.github.io/Watmonitor/             |*/
+/*|Test web interface: https://hladinomer.eu/?lang=en               |*/
+
+// Simulate project at: https://wokwi.com/projects/445828206402304001
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include "pico/stdlib.h"
+
+// --- CONFIGURATION ---
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* url = "https://hladinomer.eu/data.php"; // HTTPS server
+const char* token = "123456789";
+
+// Root certifikát servera
+const static char* test_root_ca PROGMEM = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
+
+
+// --- HC-SR04 PINS ---
+const int TRIG_PIN = 3;
+const int ECHO_PIN = 2;
+
+// --- WIFI CONNECTION ---
+void connectWiFi() {
+  Serial1.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial1.print(".");
+  }
+  Serial1.println("\nConnected!");
+  Serial1.print("IP Address: ");
+  Serial1.println(WiFi.localIP());
+}
+
+// --- MEASURE DISTANCE ---
+float measureDistance() {
+
+  const int samples = 10;
+  float sum = 0;
+
+  for (int i = 0; i < samples; i++) {
+
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    long duration = pulseIn(ECHO_PIN, HIGH, 30000); // timeout 30ms
+    float distance_cm = (duration * 0.0343) / 2.0;
+    Serial1.print("Measurement ");
+    Serial1.print(i + 1);
+    Serial1.print(": ");
+    Serial1.print(distance_cm);
+    Serial1.println(" cm");
+    sum += distance_cm;
+    delay(60);  // malá pauza medzi meraniami
+  }
+
+  float average = sum / samples;
+  Serial1.print("Average distance: ");
+  Serial1.print(average);
+  Serial1.println(" cm");
+  return average;
+}
+
+// --- SEND DATA TO SERVER ---
+void sendData(float value) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setCACert(test_root_ca);
+    HTTPClient https;
+
+    String payload = "hodnota=" + String(value) + "&token=" + String(token);
+
+    if (https.begin(client, url)) {
+      https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      https.addHeader("User-Agent", "PicoW-MCU/1.0");
+
+      int httpResponseCode = https.POST(payload);
+
+      if (httpResponseCode > 0) {
+        String response = https.getString();
+        Serial1.print("Status: ");
+        Serial1.println(httpResponseCode);
+        Serial1.print("Response: ");
+        Serial1.println(response);
+      } else {
+        Serial1.print("Error sending data: ");
+        Serial1.println(httpResponseCode);
+      }
+      https.end();
+    } else {
+      Serial1.println("Unable to connect to server!");
+    }
+  } else {
+    Serial1.println("WiFi not connected!");
+  }
+}
+
+void goToSleep(unsigned long ms) {
+  Serial1.println("Entering low power mode...");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  sleep_ms(ms);
+  Serial1.println("Waking up from sleep...");
+  connectWiFi();
+}
+
+void setup() {
+  Serial1.begin(115200);
+  Serial1.println("---- SYSTEM INFO ----");
+  Serial1.print("Arduino Core Version: ");
+  Serial1.println(ARDUINO);
+  Serial1.print("CPU Frequency: ");
+  Serial1.print(F_CPU / 1000000);
+  Serial1.println(" MHz");
+  Serial1.print("SDK Version: ");
+  Serial1.println(PICO_SDK_VERSION_STRING);
+  Serial1.println("---------------------");
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial1.println("Set distance via HC-SR04 sensor, you have 5 seconds");
+  delay(5000);
+  connectWiFi();
+}
+
+void loop() {
+  float distance = measureDistance();
+  sendData(distance);
+  goToSleep(300000); //5 min sleep
+}
